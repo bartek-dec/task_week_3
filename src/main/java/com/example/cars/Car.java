f297d0dea24ad7147fb6d20262fe17761f0cd630
Bartek Dec <decbartek@gmail.com>
2020-10-27 17:16:59 +0100

    added Car domain class

---
+++ src/main/java/com/example/cars/Car.java
@@ -0,0 +1,66 @@
+package com.example.cars;
+
+import java.util.Objects;
+
+public class Car {
+
+    private Long id;
+    private String mark;
+    private String model;
+    private Color color;
+
+    public Car() {
+    }
+
+    public Car(Long id, String mark, String model, Color color) {
+        this.id = id;
+        this.mark = mark;
+        this.model = model;
+        this.color = color;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return id.equals(car.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
