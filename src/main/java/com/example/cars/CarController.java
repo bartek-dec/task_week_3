100b1fef9086e8db71a82e2dcaeb333f17abc7d6
Bartek Dec <decbartek@gmail.com>
2020-10-28 16:36:08 +0100

    added links to the resources in the controller

--- src/main/java/com/example/cars/CarController.java
+++ src/main/java/com/example/cars/CarController.java
@@ -1,6 +1,8 @@
 package com.example.cars;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @RestController
 @RequestMapping("/api/cars")
@@ -21,49 +25,77 @@ public class CarController {
     }
 
     @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<List<Car>> getCars() {
+    public ResponseEntity<CollectionModel<Car>> getCars() {
         List<Car> foundCars = carService.getCars();
 
-        return foundCars.size() > 0 ? new ResponseEntity<>(foundCars, HttpStatus.OK)
+        foundCars.forEach(car -> car.addIf(!car.hasLinks(), () -> linkTo(CarController.class).slash(car.getId()).withSelfRel()));
+        Link link = linkTo(CarController.class).withSelfRel();
+        CollectionModel<Car> resources = CollectionModel.of(foundCars, link);
+
+        return foundCars.size() > 0 ? new ResponseEntity<>(resources, HttpStatus.OK)
                 : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<Car> getCarById(@PathVariable Long id) {
+        Link link = linkTo(CarController.class).slash(id).withSelfRel();
 
-        return carService.getCarById(id).map(car -> new ResponseEntity<>(car, HttpStatus.OK))
+        return carService.getCarById(id).map(car -> new ResponseEntity<>(car.addIf(!car.hasLinks(), () -> link), HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
 
     }
 
     @GetMapping(value = "/color/{color}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<List<Car>> getCarsByColor(@PathVariable String color) {
+    public ResponseEntity<CollectionModel<Car>> getCarsByColor(@PathVariable String color) {
         List<Car> foundCars = carService.getCarsByColor(color);
 
-        return foundCars.size() > 0 ? new ResponseEntity<>(foundCars, HttpStatus.OK) :
-                new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        foundCars.forEach(car -> car.addIf(!car.hasLinks(), () -> linkTo(CarController.class).slash(car.getId()).withSelfRel()));
+        foundCars.forEach(car -> car.addIf(!car.hasLinks(), () -> linkTo(CarController.class).withRel("allColors")));
+        Link link = linkTo(CarController.class).slash("color").slash(color).withSelfRel();
+        CollectionModel<Car> resources = CollectionModel.of(foundCars, link);
+
+        return foundCars.size() > 0 ? new ResponseEntity<>(resources, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<Car> addCar(@RequestBody Car car) {
         Optional<Car> carOptional = carService.getCarById(car.getId());
 
-        return carOptional.isEmpty() ? new ResponseEntity<>(carService.addCar(car), HttpStatus.CREATED)
-                : new ResponseEntity<>(HttpStatus.CONFLICT);
+        if (carOptional.isEmpty()) {
+            carService.addCar(car);
+            Link link = linkTo(CarController.class).slash(car.getId()).withSelfRel();
+
+            return new ResponseEntity<>(car.addIf(!car.hasLinks(), () -> link), HttpStatus.CREATED);
+        }
+
+        return new ResponseEntity<>(HttpStatus.CONFLICT);
     }
 
     @PutMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<Car> updateCar(@RequestBody Car car) {
+        Optional<Car> carOptional = carService.updateCar(car);
 
-        return carService.updateCar(car).map(e -> new ResponseEntity<>(e, HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        if (carOptional.isPresent()) {
+            Link link = linkTo(CarController.class).slash(car.getId()).withSelfRel();
+
+            return new ResponseEntity<>(car.addIf(!car.hasLinks(), () -> link), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PatchMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<Car> modifyCar(@PathVariable Long id, @RequestBody Car car) {
+        Optional<Car> carOptional = carService.modifyCar(id, car);
 
-        return carService.modifyCar(id, car).map(e -> new ResponseEntity<>(e, HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        if (carOptional.isPresent()) {
+            Car modifiedCar = carOptional.get();
+            Link link = linkTo(CarController.class).slash(id).withSelfRel();
+
+            return new ResponseEntity<>(modifiedCar.addIf(!modifiedCar.hasLinks(), () -> link), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
